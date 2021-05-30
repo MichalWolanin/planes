f3fba7b89eb9661ec6873e57e159975172e29772
Michal <michalwolanin02@gmail.com>
2021-05-30 19:36:14 +0200

    Dodaj linijkę

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,14 @@ import {DashboardComponent} from "./core/dashboard/dashboard.component";
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent},
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      { path: '', redirectTo: 'flights', pathMatch: 'full' },
+      { path: 'flights', component: LoginComponent },
+    ]
+  },
 ];
 
 @NgModule({
