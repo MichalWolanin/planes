3b1ab868c3bfe01ac502b46416c1fc5a2a1fb2e3
Michal <michalwolanin02@gmail.com>
2021-06-13 23:00:19 +0200

    Add toasts

--- src/app/flights/new-flight/new-flight.component.ts
+++ src/app/flights/new-flight/new-flight.component.ts
@@ -2,6 +2,7 @@ import {Component, ViewChild} from '@angular/core';
 import {MatDialogRef} from "@angular/material/dialog";
 import {FlightFormComponent} from "../flight-form/flight-form.component";
 import {FlightsService} from "../../core/services/flights.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-new-flight',
@@ -9,25 +10,27 @@ import {FlightsService} from "../../core/services/flights.service";
   styleUrls: ['./new-flight.component.css']
 })
 export class NewFlightComponent {
-  @ViewChild('#flightForm') flightForm: FlightFormComponent | undefined;
+  @ViewChild('flightForm') flightForm: FlightFormComponent | undefined;
 
   constructor(
     private flightsService: FlightsService,
+    private toast: MatSnackBar,
     private dialogRef: MatDialogRef<NewFlightComponent>
   ) { }
 
   createFlight() {
     console.log(this.flightForm);
-    this.flightsService.addFlight(this.flightForm!.form.value)
+    this.flightsService.addFlight(this.flightForm?.form.value)
       .then(this.onCreatingSuccess.bind(this), this.onCreatingFailure.bind(this))
   }
 
   private onCreatingSuccess() {
     this.dialogRef.close();
+    this.toast.open('Flight has been successfully created', '',{panelClass: 'toast-success'});
   }
 
-  private onCreatingFailure() {
-    console.log('some error');
+  private onCreatingFailure(error: { message: string; }) {
+    this.toast.open(error.message, '', {panelClass: 'toast-error'});
   }
 
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
-import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarConfig, MatSnackBarModule} from "@angular/material/snack-bar";
 import {MatCheckboxModule} from "@angular/material/checkbox";
 import {MatSelectModule} from "@angular/material/select";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MAT_DIALOG_DEFAULT_OPTIONS, MatDialogConfig, MatDialogModule} from "@angular/material/dialog";
 import {MatCardModule} from "@angular/material/card";
+
+const MAT_SNACK_BAR_GLOBAL_CONFIG: MatSnackBarConfig = {
+  duration: 2500,
+  verticalPosition:"bottom",
+  horizontalPosition: "center"
+}
 const MAT_DIALOG_GLOBAL_CONFIG: MatDialogConfig = {
   width: '700px',
   disableClose: true,
@@ -32,7 +38,8 @@ const MATERIAL_MODULES = [
   imports: [],
   exports: [...MATERIAL_MODULES],
   providers: [
-    {provide:MAT_DIALOG_DEFAULT_OPTIONS, useValue: MAT_DIALOG_GLOBAL_CONFIG}
+    {provide:MAT_DIALOG_DEFAULT_OPTIONS, useValue: MAT_DIALOG_GLOBAL_CONFIG},
+    {provide:MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: MAT_SNACK_BAR_GLOBAL_CONFIG}
   ]
 
 })
