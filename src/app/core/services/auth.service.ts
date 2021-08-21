58f47b89d0681e00c82b82d11487f57a61d5efd9
Michal <michalwolanin02@gmail.com>
2021-08-21 18:42:04 +0200

    I repaired user name

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -5,15 +5,21 @@ import UserInfo = firebase.UserInfo;
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
-  user: string | undefined;
+  private userData?: firebase.User | null;
 
 
   constructor(private fireAuth: AngularFireAuth) {}
 
 
   login(credentials: {email: string, password: string}) {
-    return this.fireAuth.signInWithEmailAndPassword(credentials.email, credentials.password);
+    return this.fireAuth.signInWithEmailAndPassword(credentials.email, credentials.password)
+    .then(user => this.userData = user.user);
   }
+
+  get user() {
+    return this.userData;
+  }
+
 
   register(credentials: {email: string, password: string}) {
     return this.fireAuth.createUserWithEmailAndPassword(credentials.email, credentials.password);
